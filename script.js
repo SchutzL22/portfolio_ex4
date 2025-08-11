112fec91851b548d267312297798353788d7c9fa
Lucas Schutz <lucas.schutz@aluno.fmpsc.edu.br>
2025-08-10 21:59:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Efeito de digitação na home page
+    const typed = new Typed('.typing', {
+        strings: ['Desenvolvedor de Software.', 'Programador.', 'Entusiasta de Tecnologia.'],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+});
